1feb3e4aa5aa4f9e34206278c6ac3e05f894771d
Prabushakti <127364195+Prabushakti@users.noreply.github.com>
2024-12-26 13:43:13 +0700

    Update klasifikasi_citra.py

--- src/klasifikasi_citra.py
+++ src/klasifikasi_citra.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from pathlib import Path
 import streamlit as st
 import base64
-
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 def set_background(image_file):
     with open(image_file, "rb") as f:
         img_data = f.read()
